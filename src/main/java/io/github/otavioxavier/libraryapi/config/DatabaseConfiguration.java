1292fa535c3cbdc3829f37bf1d08b43467ff8aee
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-05 21:06:37 -0300

    :hammer: refactor[application]: adiciona pull de conexões 'Hikari' personalizado

---
+++ src/main/java/io/github/otavioxavier/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,52 @@
+package io.github.otavioxavier.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+//    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return  dataSource;
+    }
+
+    public  DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000); // 10 minutos
+        config.setConnectionTimeout(6000); // 1 minuto tentando uma conexão
+
+        config.setConnectionTestQuery("SELECT 1");
+
+
+        return new HikariDataSource(config);
+    }
+}
