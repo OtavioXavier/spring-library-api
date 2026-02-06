989bc973bd270dc4f28ae58aad00c080dd69d1ec
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-05 21:21:21 -0300

    :hammer: refactor[models-autor]: adiciona entidade

---
+++ src/main/java/io/github/otavioxavier/libraryapi/model/Autor.java
@@ -0,0 +1,28 @@
+package io.github.otavioxavier.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="autor", schema = "public")
+@Getter @Setter
+public class Autor {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name="nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name="data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name="nacionalidade", nullable = false)
+    private String nacionalidade;
+}
