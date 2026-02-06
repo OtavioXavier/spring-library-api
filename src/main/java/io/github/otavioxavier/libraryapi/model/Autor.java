5e976e72709e3210fff35913a43eb20a86dd26a0
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-05 21:33:24 -0300

    :sparkles: feat[models-autor]: adiciona conexão com entidade livros

--- src/main/java/io/github/otavioxavier/libraryapi/model/Autor.java
+++ src/main/java/io/github/otavioxavier/libraryapi/model/Autor.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -25,4 +26,7 @@ public class Autor {
 
     @Column(name="nacionalidade", nullable = false)
     private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
 }
