bd829fa0c4dc4ea15ff836a904f7cc5b79812638
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-18 22:06:16 -0300

    :test_tube: test[autor-service]: garante que salva autor

---
+++ src/test/java/io/github/otavioxavier/libraryapi/service/AutorServiceTest.java
@@ -0,0 +1,39 @@
+package io.github.otavioxavier.libraryapi.service;
+
+import io.github.otavioxavier.libraryapi.model.Autor;
+import io.github.otavioxavier.libraryapi.repository.AutorRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AutorServiceTest {
+
+    @Mock
+    private AutorRepository repository;
+
+    @InjectMocks
+    private AutorService service;
+
+    @Test
+    public void DeveSalvar() {
+        Autor autor = new Autor();
+        autor.setNome("Jhon V.");
+        autor.setNacionalidade("Brasileiro");
+        autor.setDataNascimento(LocalDate.of(1980, 1, 1));
+
+        when(repository.save(autor)).thenReturn(autor);
+        Autor autorSalvo = service.saveAutor(autor);
+
+        assertNotNull(autorSalvo);
+
+    }
+}
