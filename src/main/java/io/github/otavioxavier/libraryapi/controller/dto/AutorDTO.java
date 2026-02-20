4968cec229eba72b4da26425262b2894ff09ae82
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-20 17:21:54 -0300

    :sparkles: feat[autor]: adiciona validação para tamanho máximo e mínimo de campos

--- src/main/java/io/github/otavioxavier/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/otavioxavier/libraryapi/controller/dto/AutorDTO.java
@@ -3,15 +3,18 @@ package io.github.otavioxavier.libraryapi.controller.dto;
 import io.github.otavioxavier.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record AutorDTO(
         @NotBlank(message = "Nome é obrigatório")
+        @Size(min = 2, max = 100, message = "Tamanho válido de nome: 2 ˜ 100")
         String nome,
         @NotNull(message = "Data de nascimento é obrigatório")
         LocalDate dataNascimento,
         @NotBlank(message = "Nacionalidade é obrigatório")
+        @Size(min = 2, max = 50, message = "Tamanho válido da nacionalidade: 2 ˜ 50")
         String nacionalidade) {
 
     public Autor mapearParaAutor() {
