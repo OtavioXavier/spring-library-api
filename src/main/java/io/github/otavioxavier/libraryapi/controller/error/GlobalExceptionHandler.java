07b7146ace2b7e72b95e75b4a01f00be22836dbb
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-20 16:57:12 -0300

    :sparkles: feat[autor]: adiciona mensagem de erro as requisições com erro de obrigatóriedade

---
+++ src/main/java/io/github/otavioxavier/libraryapi/controller/error/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package io.github.otavioxavier.libraryapi.controller.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErroResposta handleMethodArgumentNotValidException(final MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getFieldErrors();
+
+        List<ErroCampo> erros = fieldErrors
+                .stream()
+                .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ErroResposta(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação", erros);
+    }
+}
