01b8f792495be94b583a207c9ad35a7b89716bca
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-20 13:39:18 -0300

    :sparkles: feat[autor-repository]: implementa métodos jpa para pesquisa por nome, nacionalidade e data de nascimento

--- src/main/java/io/github/otavioxavier/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/otavioxavier/libraryapi/repository/AutorRepository.java
@@ -3,7 +3,9 @@ package io.github.otavioxavier.libraryapi.repository;
 import io.github.otavioxavier.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
@@ -11,4 +13,6 @@ public interface AutorRepository extends JpaRepository<Autor, UUID> {
     List<Autor> findByNome(String nome);
     List<Autor> findByNacionalidade(String nacionalidade);
     List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
+    Optional<Autor> findByNomeAndNacionalidadeAnDataNascimento(String nome, String nacionalidade, LocalDate dataNascimento);
 }
