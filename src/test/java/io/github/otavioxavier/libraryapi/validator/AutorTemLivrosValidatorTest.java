b338391efefde0dc21beb81304f4a2de8a58b064
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-20 15:25:06 -0300

    :test_tube: refactor[autor-service-test]: adiciona uso dos validators

---
+++ src/test/java/io/github/otavioxavier/libraryapi/validator/AutorTemLivrosValidatorTest.java
@@ -0,0 +1,52 @@
+package io.github.otavioxavier.libraryapi.validator;
+
+import io.github.otavioxavier.libraryapi.exception.OperacaoNaoPermitidaException;
+import io.github.otavioxavier.libraryapi.model.Autor;
+import io.github.otavioxavier.libraryapi.repository.LivroRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AutorTemLivrosValidatorTest {
+
+    @Mock
+    private LivroRepository livroRepository;
+
+    @InjectMocks
+    private AutorTemLivrosValidator validator;
+
+    @Test
+    void devePermitirExclusaoQuandoNaoPossuirLivros() {
+        Autor autor = new Autor();
+        autor.setId(UUID.randomUUID());
+
+        when(livroRepository.existsByAutor(autor)).thenReturn(false);
+
+        assertDoesNotThrow(() -> validator.validar(autor));
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoPossuirLivros() {
+        Autor autor = new Autor();
+        autor.setId(UUID.randomUUID());
+
+        when(livroRepository.existsByAutor(autor)).thenReturn(true);
+
+        assertThrows(
+                OperacaoNaoPermitidaException.class,
+                () -> validator.validar(autor)
+        );
+
+        verify(livroRepository).existsByAutor(autor);
+    }
+}
