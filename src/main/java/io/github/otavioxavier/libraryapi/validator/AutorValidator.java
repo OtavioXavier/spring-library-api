f2d630bd353921dbbfd696e580e5c250252ce3f1
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-20 13:34:11 -0300

    :sparkles: feat[autor]: adiciona validator para autor

---
+++ src/main/java/io/github/otavioxavier/libraryapi/validator/AutorValidator.java
@@ -0,0 +1,33 @@
+package io.github.otavioxavier.libraryapi.validator;
+
+import io.github.otavioxavier.libraryapi.exception.RegistroDuplicadoException;
+import io.github.otavioxavier.libraryapi.model.Autor;
+import io.github.otavioxavier.libraryapi.repository.AutorRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AutorValidator {
+
+    private AutorRepository autorRepository;
+
+    public AutorValidator(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    public void validar(Autor autor) {
+        if(existe(autor)) {
+            throw new RegistroDuplicadoException("Autor já cadastrado!");
+        }
+    }
+
+    private boolean existe(Autor autor) {
+        Optional<Autor> autorEncontrado = autorRepository.findByNomeAndNacionalidadeAnDataNascimento(autor.getNome(), autor.getNacionalidade(), autor.getDataNascimento());
+        if(autor.getId() == null) {
+            return autorEncontrado.isPresent();
+        }
+
+        return autorEncontrado.isPresent() && !autor.getId().equals(autorEncontrado.get().getId());
+    }
+}
