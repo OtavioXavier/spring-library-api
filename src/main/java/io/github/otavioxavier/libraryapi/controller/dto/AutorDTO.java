697e38e2732140bd3da1ceafdd073e3bf6e5f231
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-20 17:24:28 -0300

    :sparkles: feat[autor]: garante que a data de nascimento estará no passado

--- src/main/java/io/github/otavioxavier/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/otavioxavier/libraryapi/controller/dto/AutorDTO.java
@@ -3,6 +3,7 @@ package io.github.otavioxavier.libraryapi.controller.dto;
 import io.github.otavioxavier.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -12,6 +13,7 @@ public record AutorDTO(
         @Size(min = 2, max = 100, message = "Tamanho válido de nome: 2 ˜ 100")
         String nome,
         @NotNull(message = "Data de nascimento é obrigatório")
+        @Past(message = "Deve ser uma data do passado")
         LocalDate dataNascimento,
         @NotBlank(message = "Nacionalidade é obrigatório")
         @Size(min = 2, max = 50, message = "Tamanho válido da nacionalidade: 2 ˜ 50")
