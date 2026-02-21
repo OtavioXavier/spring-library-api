7c06479577fa77298a88e69083ec6acb8108b5b9
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-02-21 12:59:34 -0300

    :sparkles: feat[livros]: adiciona camada de service para livros

--- src/main/java/io/github/otavioxavier/libraryapi/service/LivroService.java
+++ src/main/java/io/github/otavioxavier/libraryapi/service/LivroService.java
@@ -1,5 +1,12 @@
 package io.github.otavioxavier.libraryapi.service;
 
+import io.github.otavioxavier.libraryapi.repository.LivroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LivroService {
 
+    private final LivroRepository livroRepository;
 }
